7d54d32c133af3e088f04f6cb64d6563acba0063
Nick <allthingsmisc01@gmail.com>
2026-03-18 16:27:06 -0700

    Add resize canvas button

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const gridContainer = document.getElementById("grid-container");
-const cells = document.getElementsByClassName(".grid-box");
-createCells(gridContainer);
+const gridSize = document.querySelector("#grid-size");
 
-cells.addEventListener("mouseover", (event) => {
-    event.target.style.color = "black";
-});
-
-function createCells(container) {
-    const fragment = document.createDocumentFragment();
-    for (let i = 0; i < 256; i++) {
+function createCells(size) {
+    for (let i = 0; i < (size * size); i++) {
         const newCell = document.createElement("div");
-        newCell.classList.add('grid-box');
-        fragment.appendChild(newCell);
+        newCell.classList.add('cell');
+        gridContainer.appendChild(newCell);
     }
-    container.appendChild(fragment);
 }
+
+function removeCells() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+}
+
+function newCanvas() {
+    let newInput = prompt("Input a number between 1-100");
+    if(newInput < 1 || newInput > 100) {
+        return alert("Invalid input");
+    }
+    removeCells();
+    createCells(newInput);
+}
+
+gridSize.addEventListener("click", newCanvas);
+
+createCells(16);
 
